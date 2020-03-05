247e803ece24594b74e3b0646e4df080f6ad4fd3
Anel <anel.drocic@gmail.com>
2020-03-04 20:10:30 -0600

    dude its fixed

--- Client/src/Services/API/api.ts
+++ Client/src/Services/API/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const API = axios.create({
     baseURL: `${process.env.baseURL ?? 'http://localhost:8080/app'}/api`,
