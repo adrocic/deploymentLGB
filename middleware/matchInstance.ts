620f4394cf2c960c451a1b88c22a78dc8ca5bbf0
Anel <anel.drocic@gmail.com>
2020-03-05 13:03:53 -0600

    aksdhfasd

--- middleware/matchInstance.ts
+++ middleware/matchInstance.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import '../env.ts';
 
+console.log('from matchinstance: ' + process.env.API_KEY);
+
 const matchInstance = axios.create({
     baseURL: 'https://na1.api.riotgames.com/lol/match/v4',
     timeout: 5000,
